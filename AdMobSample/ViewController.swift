cd9f05292795280dd1544af8531b32253c134a32
Mohsin <muhammad.mohsin.991@gmail.com>
2015-09-21 18:10:10 +0500

    upload

--- AdMobSample/ViewController.swift
+++ AdMobSample/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, GADBannerViewDelegate {
+
+    @IBOutlet weak var bannerView: GADBannerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        println("Google Mobile Ads SDK version: " + GADRequest.sdkVersion())
+        
+        // custom Ad
+//        self.bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+
+        // My Ad
+        self.bannerView.adUnitID = "ca-app-pub-9005408608667473/3327145746"
+        
+        self.bannerView.adSize = kGADAdSizeBanner
+        self.bannerView.delegate = self
+        self.bannerView.rootViewController = self
+        self.bannerView.loadRequest(GADRequest())
     }
 
     override func didReceiveMemoryWarning() {
